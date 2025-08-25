c0cfbd0618b5fb4511bb9c5a291396aa2e04198a
LucasV03 <veralucas708@gmail.com>
2025-08-25 20:08:59 -0300

    carpetasdemodulos

---
+++ src/app/clientes/layout.tsx
@@ -0,0 +1,3 @@
+export default function clientesLayout({ children }: { children: React.ReactNode }) {
+    return <>{children}</>;
+}   
---
+++ src/app/proveedores/layout.tsx
@@ -0,0 +1,3 @@
+export default function ProveedoresLayout({ children }: { children: React.ReactNode }) {
+    return <>{children}</>;
+}   
